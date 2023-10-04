0a3bd088093cbf3102265862d239cde6f49f96cc
PGoranov666 <145895566+PGoranov666@users.noreply.github.com>
2023-10-04 19:13:56 +0100

    Create 04_Sum_of_Chars.py

---
+++ Data Types and Variables - Exercise/04_Sum_of_Chars.py
@@ -0,0 +1,8 @@
+number_of_lines = int(input())
+total_sum = 0
+for characters in range(number_of_lines):
+    current_character = input()
+    ord(current_character)
+    total_sum += ord(current_character)
+
+print(f"The sum equals: {total_sum}")
