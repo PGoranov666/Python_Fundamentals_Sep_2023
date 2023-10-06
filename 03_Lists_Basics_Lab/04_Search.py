219e272b94da7e862a7c712f9f14f082aa09f496
PGoranov666 <145895566+PGoranov666@users.noreply.github.com>
2023-10-06 08:40:32 +0100

    Update 04_Search.py
    2 decisions

--- 03_Lists_Basics_Lab/04_Search.py
+++ 03_Lists_Basics_Lab/04_Search.py
@@ -1,5 +1,5 @@
 number = int(input())
-given_word = input()
+magic_word = input()
 
 my_list = []
 
@@ -10,6 +10,22 @@ print(my_list)
 
 for i in range(len(my_list) -1, -1,-1):
     element = my_list[i]
-    if given_word not in element:
+    if magic_word not in element:
         my_list.remove(element)
 print(my_list)
+
+# number = int(input())
+# magic_word = input()
+#
+# my_list = []
+#
+# for i in range(number):
+#     current_string = input()
+#     my_list.append(current_string)
+# print(my_list)
+#
+# filtered_list = []
+# for current_string in my_list:
+#     if magic_word in current_string:
+#         filtered_list.append(current_string)
+# print(filtered_list)
