a9815ddbea67504e760f36941b82ab1d63e1ae28
PGoranov666 <145895566+PGoranov666@users.noreply.github.com>
2023-10-06 09:56:33 +0100

    Update 05_Numbers_Filter.py
    2 Decisions

--- 03_Lists_Basics_Lab/05_Numbers_Filter.py
+++ 03_Lists_Basics_Lab/05_Numbers_Filter.py
@@ -1,31 +1,56 @@
+# n = int(input())
+# numbers_list = []
+#
+# for _ in range(n):
+#     number = int(input())
+#     numbers_list.append(number)
+#
+# command = input()
+# filtered_list = []
+#
+# if command == 'even':
+#     for num in numbers_list:
+#         if num % 2 == 0:
+#             filtered_list.append(num)
+#
+# if command == 'odd':
+#     for num in numbers_list:
+#         if num % 2 != 0:
+#             filtered_list.append(num)
+#
+# if command == 'positive':
+#     for num in numbers_list:
+#         if num >= 0:
+#             filtered_list.append(num)
+#
+# if command == 'negative':
+#     for num in numbers_list:
+#         if num < 0:
+#             filtered_list.append(num)
+#
+# print(filtered_list)
+
+# ADVANCED LEVEL DECISION
+
 n = int(input())
-numbers_list = []
 
-for _ in range(n):
-    number = int(input())
-    numbers_list.append(number)
+COMMAND_EVEN = "even"
+COMMAND_ODD = "odd"
+COMMAND_POSITIVE = "positive"
+COMMAND_NEGATIVE = "negative"
 
-command = input()
+numbers_list = [int(input()) for i in range(n)]
+
 filtered_list = []
 
-if command == 'even':
-    for num in numbers_list:
-        if num % 2 == 0:
-            filtered_list.append(num)
+command = input()
+for num in numbers_list:
+    filtered_command = ((command == COMMAND_EVEN and num % 2 == 0) or
+                        (command == COMMAND_ODD and num % 2 != 0) or
+                        (command == COMMAND_POSITIVE and num >= 0) or
+                        (command == COMMAND_NEGATIVE and num < 0))
 
-if command == 'odd':
-    for num in numbers_list:
-        if num % 2 != 0:
-            filtered_list.append(num)
-
-if command == 'positive':
-    for num in numbers_list:
-        if num >= 0:
-            filtered_list.append(num)
-
-if command == 'negative':
-    for num in numbers_list:
-        if num < 0:
-            filtered_list.append(num)
+    if filtered_command:
+        filtered_list.append(num)
 
 print(filtered_list)
