1a06e73736df26c239c1a4e7d3259b4c0ed57589
PGoranov666 <145895566+PGoranov666@users.noreply.github.com>
2023-10-21 07:00:08 +0100

    Update 03_Calculations.py
    2 decisions ( 2nd is with dictionary)

--- 04_Functions_Lab/03_Calculations.py
+++ 04_Functions_Lab/03_Calculations.py
@@ -1,12 +1,28 @@
+# def calculate_result(operator, num1, num2):
+#     if operator == 'multiply':
+#         return num1 * num2
+#     elif operator == 'divide':
+#         return int(num1 / num2)
+#     elif operator == 'add':
+#         return num1 + num2
+#     elif operator == 'subtract':
+#         return num1 - num2
+#
+# operator = input()
+# num1 = int(input())
+# num2 = int(input())
+#
+# result = calculate_result(operator, num1, num2)
+# print(result)
+
+
 def calculate_result(operator, num1, num2):
-    if operator == 'multiply':
-        return num1 * num2
-    elif operator == 'divide':
-        return int(num1 / num2)
-    elif operator == 'add':
-        return num1 + num2
-    elif operator == 'subtract':
-        return num1 - num2
+    return {
+        "multiply": num1 * num2,
+        "divide": int(num1 / num2),
+        "add": num1 + num2,
+        "subtract": num1 - num2,
+    }.get(operator)
 
 operator = input()
 num1 = int(input())
