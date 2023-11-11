e46ed8831b242da8f6a5c9fb9f3c426f8111c0f2
PGoranov666 <145895566+PGoranov666@users.noreply.github.com>
2023-11-11 15:10:52 +0000

    Update 02_Repeat_Strings.py

--- 08_Text_Processing_Lab/02_Repeat_Strings.py
+++ 08_Text_Processing_Lab/02_Repeat_Strings.py
@@ -1,7 +1,9 @@
 strings = input().split()
-result = ""
-
-for word in strings:
-    length = len(word)
-    result += word * length
-print(result)
+# result = ""
+#
+# for word in strings:
+#     length = len(word)
+#     result += word * length
+# print(result)
+result = [txt * len(txt) for txt in strings]
+print("".join(result))
